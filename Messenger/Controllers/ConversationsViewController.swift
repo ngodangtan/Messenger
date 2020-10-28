a1b18c3c6fe5bd5b7c3aa1f9d828b74d5ba86d1c
ngodangtan1994@gmail.com <ngodangtan@MacBook-Pro-cua-Ngo.local>
2020-10-28 14:00:53 +0700

    create project

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Ngo Dang tan on 10/28/20.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
