7eeec5c5a0343bed3614e5b69f336107ed1a65d0
ngodangtan1994@gmail.com <ngodangtan@MacBook-Pro-cua-Ngo.local>
2020-10-29 10:59:09 +0700

    add login and register screen

--- Messenger/Controllers/PhotoViewerViewController.swift
+++ Messenger/Controllers/PhotoViewerViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class PhotoViewerViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - Selectors
     
 
     /*
